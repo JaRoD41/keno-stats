81389c22dab370eb1fc6150a73bd5a538f264367
JaRoD41 <laurent.trinco@gmail.com>
2023-09-09 12:00:20 +0200

    update style

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,20 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <main>
         <div class="container">
             <h1>KenoStats</h1>
-            <div class="stats-container">
+            <div class="form-container">
             <div class="form-zone">
-            <input type="text" class="csvInput" placeholder="entrez le csv">
-            <button class="submitButton">Envoyer</button>
+            <input type="text" class="csvInput" placeholder="Entrez le csv">
+            <button class="submitCsv">Envoyer</button>
             </div>
-            
+            <div class="form-zone">
+            <input type="text" class="numberInput" placeholder="Entrez la boule">
+            <button class="submitNumber">Envoyer</button>
+            </div>
+            </div>
             <div class="results">
-                <span class="result-text">numéro du tirage :</span>
+                <span class="result-text">Détails du tirage :</span>
                 <div class="result-number"></div>
-            </div>
+            
             </div>
         </div>
     </main>
