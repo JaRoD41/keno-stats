90fda1604168bdcdc1c52b9ae5f69513eb3355e9
JaRoD41 <laurent.trinco@gmail.com>
2023-09-17 16:06:26 +0200

    add api call function

---
+++ scripts/api.ts
@@ -0,0 +1,12 @@
+export const getNumbersHistory = async () => {
+    try {
+        const apiResponse = await fetch(
+					'https://www.fdj.fr/api/service-draws/v1/games/keno/draws?include=results,ranks,addons&drawn_at='
+				)
+        const data = await apiResponse.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
+}
+
