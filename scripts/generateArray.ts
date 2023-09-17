03f7bde9ac06a9f71d622f1c6cfec4043d5b3e11
JaRoD41 <laurent.trinco@gmail.com>
2023-09-17 21:22:29 +0200

    add formattedDate

--- scripts/generateArray.ts
+++ scripts/generateArray.ts
@@ -2,26 +2,39 @@ import { getNumbersHistory } from './api'
 
 export const arrayGenerator = async () => {
 	const data = await getNumbersHistory()
-    const dataArray = document.querySelector('.historyArray') as HTMLDivElement
-    
-    dataArray.innerHTML = ''
+	const dataArray = document.querySelector('.historyArray') as HTMLDivElement
 
-    for (let i = 0; i < data.length; i++) {
-        const resultsData = data[i].results
-        const resultDate = data[i].drawn_at
-        const resultNumber = data[i].este_eid
-        
-        // Je crée un objet pour chaque tirage
-        const result = {
-					date: resultDate,
-					number: resultNumber,
-					list: [] as number[],
-				}
+	dataArray.innerHTML = ''
 
-        result.list.push(resultsData)
-        console.log('result : ', result);
-        dataArray.innerHTML += `<li>tirage N°${result.number} du : ${result.date} - ${result.list}</li>`
-    }
-	console.log('donnees : ', data)
-    
+	for (let i = 0; i < data.length; i++) {
+		const resultsData = data[i].results
+		const resultNumber = data[i].este_eid
+
+		const resultDate = new Date(data[i].drawn_at)
+
+		const day = resultDate.getDate().toString().padStart(2, '0')
+		const month = (resultDate.getMonth() + 1).toString().padStart(2, '0') // Les mois commencent à 0 en JavaScript
+		const year = resultDate.getFullYear()
+
+		const hours = resultDate.getHours().toString().padStart(2, '0')
+		const minutes = resultDate.getMinutes().toString().padStart(2, '0')
+
+		const formattedDate = `${day}/${month}/${year} à ${hours}h${minutes}`
+
+		// Je crée un objet pour chaque tirage
+		const result = {
+			date: resultDate,
+			number: resultNumber,
+			list: [] as number[],
+		}
+
+		for (let j = 0; j < resultsData.length; j++) {
+			const tirages = resultsData[j].value
+			result.list.push(tirages)
+		}
+		result.list.pop()
+		console.log('result : ', result)
+		dataArray.innerHTML += `<li>tirage N°${result.number} du : ${formattedDate} - ${result.list.join(', ')}</li>`
+	}
+	console.log('data : ', data)
 }
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,16 @@
 import '../style/normalize.css'
 import '../style/style.css'
-import { statsChecker } from '../scripts/stats'
-import {getNumbersHistory} from '../scripts/api'
-import {arrayGenerator} from '../scripts/generateArray'
+import { arrayGenerator } from '../scripts/generateArray'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <main>
-        <div class="container">
-            <h1>KenoStats</h1>
-            <div class="form-container">
-            <div class="form-zone">
-            <input type="file" id="csvInput" placeholder="Entrez le csv">
-            </div>
-            <div class="form-zone">
-            <input type="search" id="numberInput" placeholder="Entrez la boule">
-            <button class="submitNumber">Envoyer</button>
-            </div>
-            </div>
-            <div class="results">
-                <span class="result-text">Détails du tirage :</span>
-                <div class="result-number"></div>
-                <ul class="historyArray"></ul>
-            </div>
-        </div>
-    </main>
+    <div class="container">
+        <h1>KenoStats</h1> 
+        <span class="result-text">Détails des tirages :</span>
+        <ul class="historyArray"></ul>
+    </div>
+</main>
 `
 
-document.querySelector('.submitNumber')!.addEventListener('click', statsChecker)
 
 arrayGenerator()
